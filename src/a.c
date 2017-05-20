9f854779b1823b46b6198effc3218170e68367b5
Melvyn Covindarassou <melvyn.covindarassou@epitech.eu>
2017-05-20 10:54:08 +0200

    Init_main

--- src/a.c
+++
@@ -1,9 +0,0 @@
-/*
-** a.c for a in /home/covind_m/tek2carrefour/src
-** 
-** Made by Melvyn Covindarassou
-** Login   <<melvyn.covindarassou@epitech.eu>>
-** 
-** Started on  Sat May 20 10:51:02 2017 Melvyn Covindarassou
-** Last update Sat May 20 10:51:03 2017 Melvyn Covindarassou
-*/
---
+++ src/main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for main in /home/covind_m/tek2carrefour/src
+** 
+** Made by Melvyn Covindarassou
+** Login   <<melvyn.covindarassou@epitech.eu>>
+** 
+** Started on  Sat May 20 10:53:22 2017 Melvyn Covindarassou
+** Last update Sat May 20 10:53:27 2017 Melvyn Covindarassou
+*/
